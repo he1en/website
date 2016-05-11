297ab0ceb65c03bcb5fa1a37b8d09e6dc3e196bb
Helen Hastings <helenh@cs.stanford.edu>
2016-05-11 14:32:26 -0700

    Adjust spacing between cases in scrolling view

--- client/components/gender/view-case.jsx
+++ client/components/gender/view-case.jsx
@@ -10,25 +10,31 @@ var Cases = require('../../../public/resources/cases.js')
 var ViewCase = React.createClass({
 
   renderCase: function (caseInfo) {
-    var style = {
+    var imgStyle = {
       width: '250px',
       height: '250px'
     }
+    var style = {
+      'paddingTop': '30px',
+      'paddingBottom': '30px'
+    }
     return (
       <a className='linklessAnchor' name={caseInfo.id}>
-        <h4>{caseInfo.name}</h4>
-        <Grid>
-          <Row>
-            <Col xs={ 6 } md={ 4 }>
-              <Image style={style}
-                     src={'/img/cases/' + caseInfo.id + '.png'}
-                     rounded />
-            </Col>
-            <Col xs={6} md={8}>
-              <p>{caseInfo.description}</p>
-            </Col>
-          </Row>
-        </Grid>
+        <div style={style}>
+          <h4>{caseInfo.name}</h4>
+          <Grid>
+            <Row>
+              <Col xs={ 6 } md={ 4 }>
+                <Image style={imgStyle}
+                       src={'/img/cases/' + caseInfo.id + '.png'}
+                       rounded />
+              </Col>
+              <Col xs={6} md={8}>
+                <p>{caseInfo.description}</p>
+              </Col>
+            </Row>
+          </Grid>
+        </div>
       </a>
     )
   },
