8282a88f8b167477c90a9f19df29424e8bae5e45
Helen Hastings <helenh@cs.stanford.edu>
2016-05-11 14:20:02 -0700

    Update image files and explanatory text

--- public/resources/cases.js
+++ public/resources/cases.js
@@ -10,9 +10,9 @@ module.exports = [
     description: 'The gender gap in literacy remains a hidden barrier to women’s financial inclusion among undeserved groups in many developing countries. ATMs, for instance, require a basic ability to read and understand numbers and text, making them inaccessible to illiterate groups. By integrating gender analysis into the design process, tech-designers can become aware of such barriers and create better and more gender inclusive solutions. For example, providing simple icons and verbal instructions as alternatives to text and numbers in ATMs represents an innovative example in this regard.',
   },
   {
-    id: 'biometrics',
-    name: 'Biometrics',
-    description: 'Apple released its health app, HealthKit, in 2014. The app tracks lots of things: blood pressure, steps taken, daily calories, blood alcohol content, percent body fat, respiratory rate, intake of sodium, magnesium, calcium, fiber, iodine, chromium, etc. What doesn’t it track?  Women’s menstrual cycles. Apple missed half of its market. Apple fixed and released the app in 2015. What was the cost in terms of profit, poor publicity, and team morale? \n Any biometric needs to look at physiological differences between men and women (sometimes also age or ethnicity) to insure usability and safety to customers. Physiological differences are known: Oertelt-Prigione, S.& Regitz-Zagrosek, V. (Eds.) (2012). Sex and Gender Aspects in Clinical Medicine. London: Springer Verlag; Regitz-Zagrosek, V. (Ed.) (2012). Sex and Gender Differences in Pharmacology. London: Springer Verlag; Schenck-Gustafsson, K., DeCola, P., Pfaff, D., Pisetsky, D. (Eds.) (2012); Handbook of Clinical Gender Medicine. Basel: Karger, 2012; Klein, S. & Roberts, C. (Eds.) (2015). Sex and Gender Differences in Infection and Treatments of Infectious Diseases London: Springer'
+    id: 'recruiting',
+    name: 'Recruiting',
+    description: 'In a recent experiment, researchers uncovered a gender bias in an online advertising system that delivered ads for high-paying executive positions to significantly fewer women than men, despite the use of otherwise identical profiles for women and men in the experiment.'
   },
   {
     id: 'wearables',
@@ -20,18 +20,18 @@ module.exports = [
     description: 'A tech company is planning to design a wearable to detect early signs of heart disease. Recognizing that men and women have different patterns of heart disease can enhance the usability and uptake of the device. For men’s and women’s differing patterns of heart disease:'
   },
   {
-    id: 'automobile',
-    name: 'Automobile Safety',
-    description: 'Conventional seatbelts do not fit pregnant women properly, and motor vehicle crashes are the leading cause of fetal death related to maternal trauma. Gendered innovations have led to the development of pregnant crash test dummies that enhance safety in automobile testing and design. For pregnant crash test dummies:'
-  },
-  {
     id: 'data',
     name: 'Big Data',
     description: 'Public transportation systems that conceptualized new data-collection techniques to capture the “mobility of care” (travel for child-care, elder-care, etc.) created more efficient systems. Governments and civil engineers collect data to understand how people use trains, subways, and bus systems. The gendered innovation in this case study is reconceptualizing how data are collected. Traditionally, governments collect data in eight trip categories: employment, education, shopping, leisure, and the like (see image left). People who travel solely for employment tend to travel directly from home to work and back again. \n None of these traditional trip categories captures caring work—caring for children, the elderly, or households. People who travel for employment plus caring work have different travel patterns. They tend to travel from home to daycare and on to work. Leaving work, they may stop at the food market, the dry cleaners, and daycare before returning home. Conceptualizing the "mobility of care" as a category of analysis creates more efficient transportation systems, reducing costs and enhancing the quality of life.'
   },
   {
+    id: 'automobile',
+    name: 'Automobile Safety',
+    description: 'Conventional seatbelts do not fit pregnant women properly, and motor vehicle crashes are the leading cause of fetal death related to maternal trauma. Gendered innovations have led to the development of pregnant crash test dummies that enhance safety in automobile testing and design. For pregnant crash test dummies:'
+  },
+  {
     id: 'assistive',
-    name: 'Assistive Technologies for the Elderly',
+    name: 'Assistive Technologies',
     description: 'As the world’s population ages, robust new markets for assistive technologies are emerging. Elderly men and women may have different needs. Designing with these distinctive needs in mind helps engineers develop technologies that may yield broader markets. For more, see: '
   },
   {
@@ -50,8 +50,8 @@ module.exports = [
     description: 'Anyone who has spent time on Pinterest will not be surprised that the content tends to favor women. Indeed, The Wall Street Journal has reported that up until 2014, 71% of Pinterest’s users have been women. But more recently, the “visual discovery site” has received a 73% increase in male users. Pinterest has adapted to the change by updating its search algorithm to make the site more gender neutral. When a male user searches for watches, he now may see pins “inspired by what other guys have pinned” instead of women’s timepieces. Why the change? Much like many of the other social sites, Pinterest is looking to retain and build its male audience in order to sell more ads through its “promoted pins.”'
   },
   {
-    id: 'recruiting',
-    name: 'Recruiting',
-    description: 'In a recent experiment, researchers uncovered a gender bias in an online advertising system that delivered ads for high-paying executive positions to significantly fewer women than men, despite the use of otherwise identical profiles for women and men in the experiment.'
+    id: 'biometrics',
+    name: 'Biometrics',
+    description: 'Apple released its health app, HealthKit, in 2014. The app tracks lots of things: blood pressure, steps taken, daily calories, blood alcohol content, percent body fat, respiratory rate, intake of sodium, magnesium, calcium, fiber, iodine, chromium, etc. What doesn’t it track?  Women’s menstrual cycles. Apple missed half of its market. Apple fixed and released the app in 2015. What was the cost in terms of profit, poor publicity, and team morale? \n Any biometric needs to look at physiological differences between men and women (sometimes also age or ethnicity) to insure usability and safety to customers. Physiological differences are known: Oertelt-Prigione, S.& Regitz-Zagrosek, V. (Eds.) (2012). Sex and Gender Aspects in Clinical Medicine. London: Springer Verlag; Regitz-Zagrosek, V. (Ed.) (2012). Sex and Gender Differences in Pharmacology. London: Springer Verlag; Schenck-Gustafsson, K., DeCola, P., Pfaff, D., Pisetsky, D. (Eds.) (2012); Handbook of Clinical Gender Medicine. Basel: Karger, 2012; Klein, S. & Roberts, C. (Eds.) (2015). Sex and Gender Differences in Infection and Treatments of Infectious Diseases London: Springer'
   }
 ]
