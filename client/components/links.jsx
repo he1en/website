37fb926a16f8613a2b159015323f7f23a53bafe0
Helen Hastings <helenh@cs.stanford.edu>
2016-07-15 16:36:03 -0400

    Split into two parts for hello and links with different routes and add a button to switch between.

---
+++ client/components/links.jsx
@@ -0,0 +1,36 @@
+var React = require('react')
+var Grid = require('react-bootstrap').Grid
+var Row = require('react-bootstrap').Row
+var Col = require('react-bootstrap').Col
+var Thumbnail = require('react-bootstrap').Thumbnail
+
+var Links = React.createClass({
+
+  renderLinks: function () {
+    var linkNames = ['shepp_black', 'resume', 'github', 'linkedin']
+    var links = ['http://sheplusplus.org', '/resume', 'https://github.com/he1en',
+                 'https://linkedin.com/in/helenbhastings']
+    return linkNames.map(function (name, index) {
+      return (
+         <Col xs={ 3 } md={ 3 } key={ index }>
+            <Thumbnail className='main-link'
+              href={ links[index] } alt={ name } src={ '/img/' + name + '.png' } />
+         </Col>
+       )
+    })
+  },
+
+  render: function () {
+    return (
+      <div className='link-container'>
+        <Grid>
+          <Row>
+            { this.renderLinks() }
+          </Row>
+        </Grid>
+      </div>
+    )
+  }
+})
+
+module.exports = Links
