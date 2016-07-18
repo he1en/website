4ea7a6a47113c1ef4b8c2c6cf5ca7e7a57d4244b
Helen Hastings <helenh@cs.stanford.edu>
2016-07-18 16:56:30 -0400

    make link images consistent size and in middle of screen

--- client/components/links.jsx
+++ client/components/links.jsx
@@ -6,14 +6,14 @@ var Thumbnail = require('react-bootstrap').Thumbnail
 var Links = React.createClass({
 
   renderLinks: function () {
-    var linkNames = ['shepp_black', 'resume', 'github', 'linkedin']
+    var linkNames = ['shepp', 'resume', 'github', 'linkedin']
     var links = ['http://sheplusplus.org',
                  '/resume',
                  'https://github.com/he1en',
                  'https://linkedin.com/in/helenbhastings']
     return linkNames.map(function (name, index) {
       return (
-         <Col xs={ 6 } sm={ 6 } md={ 3 } lg={ 3 } key={ index }>
+         <Col xs={ 6 } sm={ 3 } md={ 3 } lg={ 3 } key={ index }>
             <Thumbnail className='main-link'
               href={ links[index] } alt={ name } src={ '/img/' + name + '.png' } />
          </Col>
